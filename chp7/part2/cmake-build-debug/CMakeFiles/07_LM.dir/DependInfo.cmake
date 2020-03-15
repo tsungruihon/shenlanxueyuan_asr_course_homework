a4c1698a97e54c2c831d932bb4f4f6260248726f
zeng_ruihong <zeng_ruihong@gowild.cn>
2020-03-15 10:23:00 +0800

    update

---
+++ chp7/part2/cmake-build-debug/CMakeFiles/07_LM.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/petertseng/Desktop/Programming/kaldi_test/07-LM/lab3_lm.C" "/Users/petertseng/Desktop/Programming/kaldi_test/07-LM/cmake-build-debug/CMakeFiles/07_LM.dir/lab3_lm.C.o"
+  "/Users/petertseng/Desktop/Programming/kaldi_test/07-LM/lang_model.C" "/Users/petertseng/Desktop/Programming/kaldi_test/07-LM/cmake-build-debug/CMakeFiles/07_LM.dir/lang_model.C.o"
+  "/Users/petertseng/Desktop/Programming/kaldi_test/07-LM/main.C" "/Users/petertseng/Desktop/Programming/kaldi_test/07-LM/cmake-build-debug/CMakeFiles/07_LM.dir/main.C.o"
+  "/Users/petertseng/Desktop/Programming/kaldi_test/07-LM/util.C" "/Users/petertseng/Desktop/Programming/kaldi_test/07-LM/cmake-build-debug/CMakeFiles/07_LM.dir/util.C.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ chp7/part2/cmake-build-debug/CMakeFiles/07_LM.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "07_LM"
+  "07_LM.pdb"
+  "CMakeFiles/07_LM.dir/lab3_lm.C.o"
+  "CMakeFiles/07_LM.dir/lang_model.C.o"
+  "CMakeFiles/07_LM.dir/main.C.o"
+  "CMakeFiles/07_LM.dir/util.C.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/07_LM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
